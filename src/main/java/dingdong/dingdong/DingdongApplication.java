a77e17463d4f8ee6ebc1a95e0432461a325d3c89
hyein <kimhyein7110@gmail.com>
2021-07-27 13:07:27 +0900

    first commit

---
+++ src/main/java/dingdong/dingdong/DingdongApplication.java
@@ -0,0 +1,13 @@
+package dingdong.dingdong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DingdongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DingdongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dingdong/dingdong/DingdongApplicationTests.java
@@ -0,0 +1,13 @@
+package dingdong.dingdong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DingdongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
