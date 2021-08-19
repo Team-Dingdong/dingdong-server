99fa3b4dedf6b9c92a7de67ddffe7c199a3d19af
hyein <kimhyein7110@gmail.com>
2021-08-20 00:43:08 +0900

    style: 코드 스타일 변경

--- src/main/java/dingdong/dingdong/controller/S3Controller.java
+++ src/main/java/dingdong/dingdong/controller/S3Controller.java
@@ -1,51 +1,48 @@
 package dingdong.dingdong.controller;
 
-import dingdong.dingdong.domain.user.Profile;
-import dingdong.dingdong.service.s3.S3Uploader;
 import dingdong.dingdong.domain.post.Post;
 import dingdong.dingdong.domain.post.PostRepository;
+import dingdong.dingdong.domain.user.Profile;
 import dingdong.dingdong.domain.user.ProfileRepository;
+import dingdong.dingdong.service.s3.S3Uploader;
 import dingdong.dingdong.util.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.Optional;
 
 import static dingdong.dingdong.util.exception.ResultCode.POST_NOT_FOUND;
 import static dingdong.dingdong.util.exception.ResultCode.PROFILE_NOT_FOUND;
 
 @Slf4j
+@RestController
 @RequiredArgsConstructor
-@Controller
+@RequestMapping("/api/v1/upload")
 public class S3Controller {
 
     private final PostRepository postRepository;
     private final ProfileRepository profileRepository;
     private final S3Uploader s3Uploader;
 
-    @PatchMapping("/post/image/{id}")
+    @PatchMapping("/post/{postId}")
     @ResponseBody
-    public void PostUpload(@RequestParam("data") MultipartFile file, @PathVariable Long id) throws IOException {
+    public void PostUpload(@RequestParam("data") MultipartFile file, @PathVariable Long postId) throws IOException {
         String path = s3Uploader.upload(file, "static");
 
-        Post optionalPost = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(POST_NOT_FOUND));
-        Post post = optionalPost;
+        Post post = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException(POST_NOT_FOUND));
 
         post.setImageUrl(path);
         postRepository.save(post);
     }
 
-    @PostMapping("/profile/image/{id}")
+    @PostMapping("/profile/{profileId}")
     @ResponseBody
-    public void ProfileUpload(@RequestParam("data") MultipartFile file, @PathVariable Long id) throws IOException {
+    public void ProfileUpload(@RequestParam("data") MultipartFile file, @PathVariable Long profileId) throws IOException {
         String path = s3Uploader.upload(file, "static");
 
-        Profile optionalProfile = profileRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(PROFILE_NOT_FOUND));
-        Profile profile = optionalProfile;
+        Profile profile = profileRepository.findById(profileId).orElseThrow(() -> new ResourceNotFoundException(PROFILE_NOT_FOUND));
 
         profile.setProfileImageUrl(path);
         profileRepository.save(profile);
--- src/main/java/dingdong/dingdong/domain/post/PostRepository.java
+++ src/main/java/dingdong/dingdong/domain/post/PostRepository.java
@@ -1,6 +1,5 @@
 package dingdong.dingdong.domain.post;
 
-import dingdong.dingdong.domain.user.Local;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -18,7 +17,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @Query(value = "select * from post, user where post.user_id = user.user_id and user.local1 = local1 or user.local2 = local2 and post.category_id = category_id",
             countQuery = "select count(*) from post",
             nativeQuery = true)
-    Page<Post> findByCategory_Id(Long local1, Long local2, Long category_id, Pageable pageable);
+    Page<Post> findByCategoryId(Long local1, Long local2, Long category_id, Pageable pageable);
 
-    Page<Post> findByUser_Id(Long UserID, Pageable pageable);
+    Page<Post> findByUserId(Long UserId, Pageable pageable);
 }
