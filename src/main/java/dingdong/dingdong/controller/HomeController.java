e62b3b4186d7b9f55174f03383bb23d900e5534b
hyein <kimhyein7110@gmail.com>
2021-09-09 22:13:10 +0900

    feat: chat test 페이지 추가

---
+++ src/main/java/dingdong/dingdong/controller/HomeController.java
@@ -0,0 +1,20 @@
+package dingdong.dingdong.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("")
+public class HomeController {
+
+    @GetMapping("")
+    public String test(Model model) {
+        return "test";
+    }
+}
