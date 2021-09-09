d7ac8e3038aa04e4ddde8d62ec0e0506730719ac
hyein <kimhyein7110@gmail.com>
2021-09-09 23:14:02 +0900

    fix: chat test controller 수정

--- src/main/java/dingdong/dingdong/controller/HomeController.java
+++ src/main/java/dingdong/dingdong/controller/HomeController.java
@@ -1,20 +1,13 @@
 package dingdong.dingdong.controller;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@Slf4j
-@RequiredArgsConstructor
 @Controller
-@RequestMapping("/")
 public class HomeController {
 
-    @GetMapping("")
-    public String test(Model model) {
-        return "test";
+    @GetMapping("/")
+    public String home() throws Exception {
+        return "test"; //note that this says .html
     }
 }
