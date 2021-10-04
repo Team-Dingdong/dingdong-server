95d6ff299fbade617a1ef10a563f08a06ce1c6b1
hyeinisfree <kimhyein7110@gmail.com>
2021-10-04 16:40:37 +0900

    test: RatingControllerTest 작성

---
+++ src/test/java/dingdong/dingdong/controller/RatingControllerTest.java
@@ -0,0 +1,232 @@
+package dingdong.dingdong.controller;
+
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyUris;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.relaxedResponseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dingdong.dingdong.domain.user.Auth;
+import dingdong.dingdong.domain.user.AuthRepository;
+import dingdong.dingdong.domain.user.Profile;
+import dingdong.dingdong.domain.user.ProfileRepository;
+import dingdong.dingdong.domain.user.User;
+import dingdong.dingdong.domain.user.UserRepository;
+import dingdong.dingdong.dto.auth.AuthRequestDto;
+import dingdong.dingdong.dto.auth.TokenDto;
+import dingdong.dingdong.dto.rating.RatingRequestDto;
+import dingdong.dingdong.service.auth.AuthService;
+import dingdong.dingdong.service.auth.AuthType;
+import dingdong.dingdong.service.rating.RatingType;
+import java.time.LocalDateTime;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@Transactional
+class RatingControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    AuthService authService;
+
+    @Autowired
+    AuthRepository authRepository;
+
+    @Autowired
+    ProfileRepository profileRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Value("${test.server.http.scheme}")
+    String scheme;
+    @Value("${test.server.http.host}")
+    String host;
+    @Value("${test.server.http.port}")
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        // user1 생성
+        Long id1 = 1L;
+        String phone1 = "01011111111";
+        String authNumber = "123456";
+        String requestId = "testRequestId";
+        LocalDateTime requestTime = LocalDateTime.now();
+        Auth auth = Auth.builder()
+            .id(id1)
+            .phone(phone1)
+            .authNumber(authNumber)
+            .requestId(requestId)
+            .requestTime(requestTime)
+            .build();
+
+        authRepository.save(auth);
+
+        Profile profile1 = Profile.builder()
+            .id(id1)
+            .good(0L)
+            .bad(0L)
+            .build();
+
+        String authority = "ROLE_USER";
+        User user1 = User.builder()
+            .id(id1)
+            .phone(phone1)
+            .profile(profile1)
+            .authority(authority)
+            .build();
+
+        profileRepository.save(profile1);
+        userRepository.save(user1);
+
+        // user2 생성
+        Long id2 = 1L;
+        String phone2 = "01011111111";
+
+        Profile profile2 = Profile.builder()
+            .id(id2)
+            .good(0L)
+            .bad(0L)
+            .build();
+
+        User user2 = User.builder()
+            .id(id2)
+            .phone(phone2)
+            .profile(profile2)
+            .authority(authority)
+            .build();
+
+        profileRepository.save(profile2);
+        userRepository.save(user2);
+    }
+
+    TokenDto getTokenDto() {
+        // user1의 token
+        Auth auth = authRepository.findById(1L).get();
+        AuthRequestDto authRequestDto = AuthRequestDto.builder()
+            .phone(auth.getPhone())
+            .authNumber(auth.getAuthNumber())
+            .build();
+        Map<AuthType, TokenDto> data = authService.auth(authRequestDto);
+
+        return data.get(AuthType.LOGIN);
+    }
+
+    @Test
+    @DisplayName("본인 평가 조회")
+    void getRating() throws Exception {
+        TokenDto tokenDto = getTokenDto();
+
+        mockMvc.perform(RestDocumentationRequestBuilders.get("/api/v1/rating")
+            .header(HttpHeaders.AUTHORIZATION, tokenDto.getAccessToken())
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andDo(print()).andExpect(status().isOk()).andDo(print())
+            .andDo(document("{class-name}/{method-name}",
+                preprocessRequest(modifyUris().scheme(scheme).host(host).port(port), prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestHeaders(
+                    headerWithName(HttpHeaders.AUTHORIZATION)
+                        .description("Bearer Type의 AccessToken 값")
+                ),
+                relaxedResponseFields(
+                    fieldWithPath("data.good").type(JsonFieldType.NUMBER).description("사용자 추천 수"),
+                    fieldWithPath("data.bad").type(JsonFieldType.NUMBER).description("사용자 비추천 수"),
+                    fieldWithPath("data.total").type(JsonFieldType.NUMBER)
+                        .description("사용자의 총 평가 수")
+                )
+            ));
+    }
+
+    @Test
+    @DisplayName("평가 조회")
+    void testGetRating() throws Exception {
+        TokenDto tokenDto = getTokenDto();
+
+        mockMvc.perform(RestDocumentationRequestBuilders.get("/api/v1/rating/{userId}", 2L)
+            .header(HttpHeaders.AUTHORIZATION, tokenDto.getAccessToken())
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andDo(print()).andExpect(status().isOk()).andDo(print())
+            .andDo(document("{class-name}/{method-name}",
+                preprocessRequest(modifyUris().scheme(scheme).host(host).port(port), prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestHeaders(
+                    headerWithName(HttpHeaders.AUTHORIZATION)
+                        .description("Bearer Type의 AccessToken 값")
+                ),
+                pathParameters(
+                    parameterWithName("userId").description("조회하고자 하는 사용자의 고유 아이디 값")
+                ),
+                relaxedResponseFields(
+                    fieldWithPath("data.good").type(JsonFieldType.NUMBER).description("사용자 추천 수"),
+                    fieldWithPath("data.bad").type(JsonFieldType.NUMBER).description("사용자 비추천 수"),
+                    fieldWithPath("data.total").type(JsonFieldType.NUMBER)
+                        .description("사용자의 총 평가 수")
+                )
+            ));
+    }
+
+    @Test
+    @DisplayName("평가 생성")
+    void createRating() throws Exception {
+        TokenDto tokenDto = getTokenDto();
+
+        RatingRequestDto ratingRequestDto = RatingRequestDto.builder()
+            .type(RatingType.GOOD)
+            .build();
+
+        mockMvc.perform(RestDocumentationRequestBuilders.post("/api/v1/rating/{userId}", 2L)
+            .header(HttpHeaders.AUTHORIZATION, tokenDto.getAccessToken())
+            .content(objectMapper.writeValueAsString(ratingRequestDto))
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andDo(print()).andExpect(status().is2xxSuccessful()).andDo(print())
+            .andDo(document("{class-name}/{method-name}",
+                preprocessRequest(modifyUris().scheme(scheme).host(host).port(port), prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestHeaders(
+                    headerWithName(HttpHeaders.AUTHORIZATION)
+                        .description("Bearer Type의 AccessToken 값")
+                ),
+                pathParameters(
+                    parameterWithName("userId").description("평가하고자 하는 사용자의 고유 아이디 값")
+                ),
+                requestFields(
+                    fieldWithPath("type").type(JsonFieldType.STRING).description("GOOD 또는 BAD")
+                )
+            ));
+    }
+}
