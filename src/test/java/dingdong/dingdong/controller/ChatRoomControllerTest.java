1dfdd4ae1d8025dd817045c10f68955f48c17caa
hyeinisfree <kimhyein7110@gmail.com>
2021-10-04 16:49:39 +0900

    test: ChatRoomControllerTest 작성

---
+++ src/test/java/dingdong/dingdong/controller/ChatRoomControllerTest.java
@@ -0,0 +1,165 @@
+package dingdong.dingdong.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dingdong.dingdong.domain.user.Auth;
+import dingdong.dingdong.domain.user.AuthRepository;
+import dingdong.dingdong.domain.user.Profile;
+import dingdong.dingdong.domain.user.ProfileRepository;
+import dingdong.dingdong.domain.user.User;
+import dingdong.dingdong.domain.user.UserRepository;
+import dingdong.dingdong.dto.auth.AuthRequestDto;
+import dingdong.dingdong.dto.auth.TokenDto;
+import dingdong.dingdong.service.auth.AuthService;
+import dingdong.dingdong.service.auth.AuthType;
+import java.time.LocalDateTime;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@Transactional
+class ChatRoomControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    AuthService authService;
+
+    @Autowired
+    AuthRepository authRepository;
+
+    @Autowired
+    ProfileRepository profileRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Value("${test.server.http.scheme}")
+    String scheme;
+    @Value("${test.server.http.host}")
+    String host;
+    @Value("${test.server.http.port}")
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        // user1 생성
+        Long id1 = 1L;
+        String phone1 = "01011111111";
+        String authNumber = "123456";
+        String requestId = "testRequestId";
+        LocalDateTime requestTime = LocalDateTime.now();
+        Auth auth = Auth.builder()
+            .id(id1)
+            .phone(phone1)
+            .authNumber(authNumber)
+            .requestId(requestId)
+            .requestTime(requestTime)
+            .build();
+
+        authRepository.save(auth);
+
+        Profile profile1 = Profile.builder()
+            .id(id1)
+            .good(0L)
+            .bad(0L)
+            .build();
+
+        String authority = "ROLE_USER";
+        User user1 = User.builder()
+            .id(id1)
+            .phone(phone1)
+            .profile(profile1)
+            .authority(authority)
+            .build();
+
+        profileRepository.save(profile1);
+        userRepository.save(user1);
+
+        // user2 생성
+        Long id2 = 2L;
+        String phone2 = "01022222222";
+
+        Profile profile2 = Profile.builder()
+            .id(id2)
+            .good(0L)
+            .bad(0L)
+            .build();
+
+        User user2 = User.builder()
+            .id(id2)
+            .phone(phone2)
+            .profile(profile2)
+            .authority(authority)
+            .build();
+
+        profileRepository.save(profile2);
+        userRepository.save(user2);
+    }
+
+    TokenDto getTokenDto() {
+        // user1의 token
+        Auth auth = authRepository.findById(1L).get();
+        AuthRequestDto authRequestDto = AuthRequestDto.builder()
+            .phone(auth.getPhone())
+            .authNumber(auth.getAuthNumber())
+            .build();
+        Map<AuthType, TokenDto> data = authService.auth(authRequestDto);
+
+        return data.get(AuthType.LOGIN);
+    }
+
+
+    @Test
+    void findChatRooms() {
+    }
+
+    @Test
+    void findChatRoomByRoomId() {
+    }
+
+    @Test
+    void findUsersByRoomId() {
+    }
+
+    @Test
+    void enterChatRoom() {
+    }
+
+    @Test
+    void quitChatRoom() {
+    }
+
+    @Test
+    void findChatMessagesByRoomId() {
+    }
+
+    @Test
+    void createChatPromise() {
+    }
+
+    @Test
+    void updateChatPromise() {
+    }
+
+    @Test
+    void findChatPromiseByPostId() {
+    }
+
+    @Test
+    void createVotePromsie() {
+    }
+}
