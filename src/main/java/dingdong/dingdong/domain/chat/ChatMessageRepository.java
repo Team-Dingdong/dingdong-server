30d1071ca876454a047edc89633aaca271457c16
younboyoung <boyung1021@naver.com>
2021-10-26 13:24:55 +0900

    [server][feat] post delete 기능 수정

--- src/main/java/dingdong/dingdong/domain/chat/ChatMessageRepository.java
+++ src/main/java/dingdong/dingdong/domain/chat/ChatMessageRepository.java
@@ -1,7 +1,10 @@
 package dingdong.dingdong.domain.chat;
 
+import dingdong.dingdong.domain.user.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
 
+    List<ChatMessage> findAllByChatRoom(ChatRoom chatRoom);
 }
